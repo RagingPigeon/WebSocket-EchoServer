1702859a11c42f61786a4922be1186e7e0533f79
Austin Farrell <farrellrage@gmail.com>
2024-11-22 13:26:08 -0500

    Updated the message structures

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod messages;
 use axum::{
+    extract::ws::{
+        Message,
+        WebSocketUpgrade,
+        WebSocket,
+    },
     http::header::HeaderMap,
     response::Json as response_json,
+    response::Response,
     Router,
     routing::get,
     routing::post,
@@ -17,28 +23,42 @@ use messages::{
     TimeFilterResponse
 };
 use rand::Rng;
+use std::{
+    thread,
+    time::{
+        Duration,
+        self,
+    }
+};
+use thread_id;
 use tracing::{event, Level};
 use tracing_subscriber;
 use uuid::Uuid;
 
-pub const WS_UNCLASSIFIED_URL: &str = "wss://localhost:3030/root";
+pub const WS_UNCLASSIFIED_URL: &str = "wss://localhost/root";
 pub const DEFAULT_SERVE_IP: &str = "0.0.0.0";
 pub const DEFAULT_SERVE_PORT: i32 = 80;
 
 pub const UNCLASSIFIED_STRING: &str = "UNCLASSIFIED";
+pub const TEST_ROOM_NAME: &str = "edge-view-test-room";
 pub const TEST_DOMAIN_ID: &str = "chatsurferxmppunclass"; 
+pub const TEST_KEYWORD: &str = "Antediluvian";
 
 pub const GET_API_KEY_ROUTE: &str = "/api/auth/key";
 pub const MESSAGES_ROUTE: &str = "/api/chat/messages/chatsurferxmppunclass/edge-view-test-room";
 pub const NEW_MESSAGE_ROUTE: &str = "/api/chatserver/message";
 pub const SEARCH_MESSAGES_ROUTE: &str = "/api/chat/messages/search";
 
+pub const WS_SINGLE_ROOM_ROUTE: &str = "/topic/chat-messages-room/chatsurferxmppunclass/edge-view-test-room";
 
-fn build_region_array
-(
-    seed: i32,
+pub const SECONDS_BETWEEN_WEBSOCKET_UPDATE: u64 = 1;
+
+pub const MAX_REGIONS: usize = 5;
+
+fn build_region_array(
+    seed:   i32,
     length: usize
-) -> [messages::RegionSchema; messages::MAX_REGIONS] {
+) -> Vec<messages::RegionSchema> {
     let mut temp_vector: Vec<messages::RegionSchema> = Vec::new();
     let mut index: usize = 0;
 
@@ -47,57 +67,50 @@ fn build_region_array
         index += 1;
     }
 
-    temp_vector.try_into().unwrap_or_else(|temp_vector: Vec<RegionSchema>| panic!("Expected length of {} but it was {}", messages::MAX_REGIONS, temp_vector.len()))
+    temp_vector
 }
 
 fn build_geotag(seed: i32) -> messages::GeoTagSchema {
     messages::GeoTagSchema {
-        anchorEnd:      seed as i64,
-        anchorStart:    seed as i64,
-        anchorText:     String::from(format!("Anchor text for GeoTag {}", seed)),
+        anchor_end:      seed as i64,
+        anchor_start:    seed as i64,
+        anchor_text:     String::from(format!("Anchor text for GeoTag {}", seed)),
         confidence:     seed as f32,
         location:       messages::LocationSchema::init(
                             1.0,
                             messages::LocationType::Point),
         regions:        build_region_array(
                             seed,
-                            messages::MAX_REGIONS),
+                            MAX_REGIONS),
         r#type: String::from(format!("PAL"))
     }
 }
 
-fn build_geotag_array(seed: i32) -> [messages::GeoTagSchema; messages::MAX_MESSAGE_GEOTAGS] {
-
-    let new_array: [messages::GeoTagSchema; messages::MAX_MESSAGE_GEOTAGS] = [
-        build_geotag(seed),
-    ];
-
-    new_array
+fn build_geotag_array(seed: i32) -> Vec<messages::GeoTagSchema> {
+    vec!(build_geotag(seed))
 }
 
-fn build_chat_message
-(
+fn build_chat_message(
     seed: i32,
     new_name: &str,
     additional_text: &str,
 ) -> messages::ChatMessageSchema {
 
-    let new_message: messages::ChatMessageSchema = messages::ChatMessageSchema {
+    messages::ChatMessageSchema {
         classification: String::from(UNCLASSIFIED_STRING),
-        domainId:       String::from(TEST_DOMAIN_ID),
-        geoTags:        build_geotag_array(seed),
-        id:             Uuid::new_v4(),
-        roomName:       String::from("Test room"),
+        domain_id:      String::from(TEST_DOMAIN_ID),
+        geo_tags:       Some(build_geotag_array(seed)),
+        id:             Uuid::new_v4().to_string(),
+        room_name:      String::from(TEST_ROOM_NAME),
         sender:         String::from(new_name),
         text:           String::from(format!("{}{}", 
             "This is some test message text.",
             additional_text)),
-        threadId:       Uuid::new_v4(),
+        thread_id:      Some(Uuid::new_v4().to_string()),
         timestamp:      Utc::now().to_string(),
-        userId:         Uuid::new_v4()
-    };
-
-    new_message
+        user_id:        Uuid::new_v4().to_string(),
+        private:        false,
+    }
 } //end build_chat_message
 
 fn build_get_messages_response() -> messages::GetChatMessagesResponse {
@@ -106,13 +119,13 @@ fn build_get_messages_response() -> messages::GetChatMessagesResponse {
     messages.push(build_chat_message(
         25,
         "Austin",
-        ""
+        TEST_KEYWORD
     ));
     messages.push(build_chat_message(4, "Tyler", ""));
-    messages.push(build_chat_message(7, "Joe", "test_keyword"));
+    messages.push(build_chat_message(7, "Joe", TEST_KEYWORD));
     messages.push(build_chat_message(9, "Jeremy", ""));
     messages.push(build_chat_message(2, "Trevor", ""));
-    messages.push(build_chat_message(4, "Justin", "test_keyword"));
+    messages.push(build_chat_message(4, "Justin", TEST_KEYWORD));
     messages.push(build_chat_message(97856, "Ryan", ""));
     messages.push(build_chat_message(123, "Joseph", ""));
     messages.push(build_chat_message(432, "Rita", ""));
@@ -121,7 +134,10 @@ fn build_get_messages_response() -> messages::GetChatMessagesResponse {
 
     messages::GetChatMessagesResponse {
         classification: messages::UNCLASSIFIED_STRING.to_string(),
-        messages: messages
+        messages: messages,
+        domain_id: String::from(TEST_DOMAIN_ID),
+        room_name: String::from(TEST_ROOM_NAME),
+        private: false,
     }
 }
 
@@ -153,14 +169,13 @@ async fn handle_get_api_key() -> (StatusCode, String) {
         dn:             String::from("CN=Austin,O=Nine Hill Technology,ST=New York,C=US"),
         email:          String::from("austin.farrell@ninehilltech.com"),
         key:            String::from("a7B5siy9xY1dmN"),
-        status:         messages::ApiKeyStatus::ACTIVE,
+        status:         serde_json::to_string(&messages::ApiKeyStatus::ACTIVE).unwrap(),
     };
 
     (StatusCode::OK, serde_json::to_string(&response).unwrap())
 } // end handle_get_api_key
 
-async fn handle_get_messages
-(
+async fn handle_get_messages(
     headers:    HeaderMap,
 ) -> (StatusCode, String) {
     event!(Level::DEBUG, "Received the Get Messages Request");
@@ -178,8 +193,7 @@ async fn handle_get_messages
     (StatusCode::OK, serde_json::to_string(&response).unwrap())
 }
 
-async fn handle_post_chat_message
-(
+async fn handle_post_chat_message(
     headers:    HeaderMap,
     payload:    String,
 ) -> (StatusCode, String) {
@@ -207,12 +221,12 @@ async fn handle_post_chat_message
         // 400 Bad Request case.
         1 => {
             let body = messages::ErrorCode400 {
-                fieldErrors: vec![messages::FieldErrorSchema {
-                    fieldName:          String::from("roomName"),
+                field_errors: vec![messages::FieldErrorSchema {
+                    field_name:          String::from("roomName"),
                     message:            String::from("Room name not found"),
-                    messageArguments:   [String::from("I don't know what to put here")],
-                    messageCode:        String::from("I don't know what to put here"),
-                    rejectedValue:      String::from(request.roomName)
+                    message_arguments:   vec!(String::from("I don't know what to put here")),
+                    message_code:        String::from("I don't know what to put here"),
+                    rejected_value:      String::from(request.room_name)
                 }],
                 ..Default::default()
             };
@@ -228,8 +242,7 @@ async fn handle_post_chat_message
     }
 }
 
-async fn handle_search_messages
-(
+async fn handle_search_messages(
     headers:    HeaderMap,
     payload:    String
 ) -> (StatusCode, String) {
@@ -250,15 +263,15 @@ async fn handle_search_messages
     match num {
         // 200 Successful case.
         0 => {
-            let search_results = search_messages(request.keywordFilter.unwrap().query);
+            let search_results = search_messages(request.keyword_filter.unwrap().query);
             let total: i32 = search_results.len() as i32;
 
             let body = messages::SearchChatMessagesResponse {
                 classification:     String::from(UNCLASSIFIED_STRING),
                 messages:           Some(search_results),
-                nextCursorMark:     None,
-                searchTimeFiler:    TimeFilterResponse {
-                    endDateTime:    Utc::now().to_rfc3339()
+                next_cursor_mark:     None,
+                search_time_filter:    TimeFilterResponse {
+                    end_date_time:    Utc::now().to_rfc3339()
                 },
                 total:              total,
             };
@@ -273,12 +286,12 @@ async fn handle_search_messages
                 classification: String::from(UNCLASSIFIED_STRING),
                 code:           400,
 
-                fieldErrors:    vec![messages::FieldErrorSchema {
-                    fieldName:          String::from("keywordFilter"),
+                field_errors:    vec![messages::FieldErrorSchema {
+                    field_name:          String::from("keywordFilter"),
                     message:            String::from("'*' or '?' not allowed as first character of a term"),
-                    messageArguments:   [String::from("I don't know what to put here")],
-                    messageCode:        String::from("ChatMessageSearchQueryStringIsInvalid"),
-                    rejectedValue:      String::from("**")
+                    message_arguments:   vec!(String::from("I don't know what to put here")),
+                    message_code:        String::from("ChatMessageSearchQueryStringIsInvalid"),
+                    rejected_value:      String::from("**")
                 }],
 
                 message:        String::from("The request contained 1 or more field validation errors."),
@@ -297,8 +310,46 @@ async fn handle_search_messages
 
 async fn handle_public_key_request() -> String {
     event!(Level::DEBUG, "Received the Get Public Key Request");
+
     String::from("{\"realm\":\"fmv\",\"public_key\":\"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzq/jsj5MTmOA9sW4YBJpv16yLPvznKLj3UqNXQ17WhukP5wu6GQyHMUSqNV8CAqGEA8TJpoQcpTCs8iaKxpfF1yORKdeuvCa/aJZpOw6TwsJZa1OWLONyJnOuPeZZNDUn+D7as+tS9ws7UP3AtROO8hkMS7+B3C90eXTWhZnkzEDSfDmfUxPMvYH/5yGUI4AtzbAGPMwiDOXOguXUSkV5TP7RXTZqrgHp3yvzBsbaWtjW9r4tfzXRHuGFXhlEgBdsBIzupaXrpfqIjHQXDhJ1NnI6KOQUTDi5t3VOhfZ8z6WXMPdqi/pvyzTenAshvoTR2rEti6KyLqwTdW6y1KFVQIDAQAB\",\"token-service\":\"https://app.fmvedgeview.net/keycloak/auth/realms/fmv/protocol/openid-connect\",\"account-service\":\"https://app.fmvedgeview.net/keycloak/auth/realms\",\"tokens-not-before\":0}")
 } // end handle_public_key_request
+
+async fn serve_ws_single_room(
+    mut socket: axum::extract::ws::WebSocket
+) {
+    loop {
+        // We will periodically send messages to the client to simulate events
+        // taking place within a ChatSurfer chat room.
+        thread::sleep(Duration::from_secs(SECONDS_BETWEEN_WEBSOCKET_UPDATE));
+
+        // Send a randomly generated chat message to the client.
+
+        let random_seed = rand::random::<i32>();
+
+        let message = build_chat_message(
+            random_seed.clone(),
+            "Austin",
+            random_seed.clone().to_string().as_str()
+        );
+
+        match socket.send(Message::Text(
+            message.try_to_json().unwrap()
+        )).await {
+            Ok(()) => {
+                event!(Level::DEBUG, "Successfully sent message {} to client.", random_seed);
+            }
+            Err(e) => {
+                event!(Level::ERROR, "Error - could not send the response to the client: {}", e);
+            }
+        }
+    }
+} // end serve_ws_single_room
+
+async fn serve_ws_single_room_upgrade_handler(
+    ws: WebSocketUpgrade,
+) -> Response {
+    ws.on_upgrade(|socket| serve_ws_single_room(socket))
+} // end serve_ws_single_room_upgrade_handler
 
 /*
  * This struct describes the possible arguments accepted by the
@@ -324,6 +375,17 @@ impl Args {
     }
 }
 
+async fn test() {
+
+    loop {
+        
+        event!(Level::DEBUG, "Thread {}: spinning", thread_id::get());
+        
+        thread::sleep(time::Duration::from_secs(10));
+    }
+
+}
+
 #[tokio::main]
 //-> Result<(), Box<dyn std::error::Error + Send + Sync>>
 async fn main()  {
@@ -341,11 +403,13 @@ async fn main()  {
 
 
     let test_route = Router::new()
+        .route("/auth/realms/fmv", get(handle_public_key_request))
         .route(GET_API_KEY_ROUTE, get(handle_get_api_key))
         .route(MESSAGES_ROUTE, get(handle_get_messages))
         .route(NEW_MESSAGE_ROUTE, post(handle_post_chat_message))
         .route(SEARCH_MESSAGES_ROUTE, post(handle_search_messages))
-        .route("/auth/realms/fmv", get(handle_public_key_request));
+        .route(WS_SINGLE_ROOM_ROUTE, get(serve_ws_single_room_upgrade_handler))
+        .route("/test", get(test));
 
     
     let axum_listener = tokio::net::TcpListener::bind(serve_address).await.unwrap();
